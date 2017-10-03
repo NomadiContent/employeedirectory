52771959c98ff9d483fc02cef2e2cc0dd30f9a05
Charley Montgomery <charleymontgomery@Charleys-MacBook-Pro.local>
2017-10-03 16:09:58 +0800

    adding comments for readability

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+//initialize array to hold details for modal window.
 people = [];
 
 $.ajax({
@@ -7,6 +8,7 @@ $.ajax({
   success: function(data) {
     console.log(data);
     for (i in data.results) {
+          //define attributes for items returned from api call to append to html document
           let email = "<p>" + data.results[i].email + "</p>"
           let name = "<p>" + data.results[i].name.first + " " + data.results[i].name.last + "</p>"
           let city = "<p>" + data.results[i].location.city + "</p>"
@@ -14,17 +16,25 @@ $.ajax({
           let phone = "<p>" + data.results[i].cell + "</p>"
           let dob = "<p>" + data.results[i].dob + "</p>"
           let address = "<p>" + data.results[i].location.street +  " " + data.results[i].location.city + data.results[i].location.city + data.results[i].location.postcode + "</p>"
+
+          //push necessary attributes to array so they persist and can be called and add in modal window
           people[i] =  "<div class='photo'>" + img + "</div>" + "<div class='text'>" + name + email+ city + phone + address + "</div>"
+
+          //create body of html document
           $("body").append("<div class=col-" + i  + " id="+ i +">" + img + email + " " + name + city + "</div>")
+
+          //add class to make img round
           $('img').addClass('img-circle');
 
     }
+    //on click open modal window and append biographical data saved in array.
     $('[class^=col-]').click(function(){
       $('#myModal').css('display', 'block')
       let index = $(this).attr('id');
       $('.modal-content').append('<div class="temp">'+ people[index] +'</div>')
       $('img').addClass('img-circle');
     });
+    //on close of modal window, remove biodata from element.
     $('.close').click(function() {
       $('#myModal').css("display","none");
       $('.temp').remove();
